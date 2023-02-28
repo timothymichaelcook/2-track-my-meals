6d25072bf033fb7a87efad764d595e1080d0c148
elizabeth189 <elizabethlopez0108@gmail.com>
2023-02-27 20:37:46 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 // Code that might help is below current code:
 
-const Sequelize = require('sequelize'); 
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
+let sequelize;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
+
 
 // Code that may be useful from another repo. since we are using mysql and Heroku -> (.JAWSDB_URL) Please delete code if we don't need it:
 
