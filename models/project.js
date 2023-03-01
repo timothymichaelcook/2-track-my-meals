60d97819853051fc44e83dcf934f33fa1aae26f1
Mandarinfish <66844643+Mandarinfish1@users.noreply.github.com>
2023-03-01 07:44:06 -0500

    adding code to models/project.js

--- models/project.js
+++ models/project.js
@@ -1,32 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+// P Note: Importing built-in DataTypes from sequelize documentation available online.
+const { Model, DataTypes } = require('sequelize'); 
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class Project extends Model {}           // class Meal extends Model {}
 
-Project.init(
+Project.init(                            // Meal.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    id: {                         
+      type: DataTypes.INTEGER,         
+      allowNull: false,                 //P Note: allowNull: false, -> this field cannot be left empty or null.
+      primaryKey: true,                 //P Note: primaryKey: true, ->this field will be the primaryKey for    
+      autoIncrement: true,                       // the table. The primaryKey id's each row in the table.
+    },                                  //P Note: autoIncrement: true,->Value for this row field increment +1
+    name: {                             // meal_name:  {                            // <- P code change.
+      type: DataTypes.STRING,         
+      allowNull: false,              
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    description: {                      // is_healthy: {                           // <- P code change.
+      type: DataTypes.STRING,           // type: DataTypes.BOOLEAN,                // <- P code change.
+                                        //allowNull: false,                        // <- P code change.
     },
-    description: {
-      type: DataTypes.STRING,
-    },
-    date_created: {
+    date_created: {                     // meal_date: {                           // <- P code change.
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
+    needed_funding: {                 // <- P Note: remove this model option.
+      type: DataTypes.FLOAT,          // <- P Note: remove.
+      allowNull: false,               // <- P Note: remove.
+    },                                // <- P Note: remove.
     user_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -40,8 +42,9 @@ Project.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'project',
+    modelName: 'project',             //modelName:'meal',                       // <- P code change.
   }
 );
 
-module.exports = Project;
+module.exports = Project;             //module.export = Meal;   // P Note: change file name -> Meal.js
+
