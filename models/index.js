6a7728bf1b1c5ba7c7c3e51ee6bb4bbc5ab66de8
Mandarinfish <66844643+Mandarinfish1@users.noreply.github.com>
2023-03-03 20:18:14 -0500

    updating models/index.js file, user.js file

--- models/index.js
+++ models/index.js
@@ -1,13 +1,16 @@
+// Importing ./User file and ./Project file into this file.
+
 const User = require('./User');
 const Project = require('./Project');
 
+//Using onDelete:'CASCADE to delete all associated informaton for any user who has been deleted.
 User.hasMany(Project, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
 Project.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
 });
 
 module.exports = { User, Project };
