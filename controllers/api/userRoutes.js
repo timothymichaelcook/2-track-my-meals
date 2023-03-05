50f57202650ee8ba66b82107c9546784ed63bfe5
Mandarinfish <66844643+Mandarinfish1@users.noreply.github.com>
2023-03-04 22:30:48 -0500

    updating controllers folder

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,8 @@
+// Importing {User} model data from /models folder.
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Post request to create User.
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -16,10 +18,13 @@ router.post('/', async (req, res) => {
   }
 });
 
+// router handles post request to login. Login page loads...
+// and request is made to find User email in database that matches   email entered.
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
+    // if userData does not match, then (400)error response is sent.
     if (!userData) {
       res
         .status(400)
@@ -27,8 +32,10 @@ router.post('/login', async (req, res) => {
       return;
     }
 
+    // checking match on saved pswrd in records against login password used.
     const validPassword = await userData.checkPassword(req.body.password);
 
+    // if password does not match. (400)error is sent with message.
     if (!validPassword) {
       res
         .status(400)
@@ -36,18 +43,19 @@ router.post('/login', async (req, res) => {
       return;
     }
 
+    // If user is logged in. Save changes to userData id.
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+// Post request to destroy session data when a user logs in and sends a (204) No content response. else (404) Not found status is sent.
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
+// Importing .Router from 'express'
 const router = require('express').Router();
 
+// Importing data from ./api folder.
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
+// Any requests to the '/api' sub-path (api relate) or any sub-paths under it will be handled by the apiRoutes router object.
+// Any other request routes are handled by homeRoutes.
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
