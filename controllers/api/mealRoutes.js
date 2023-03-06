245b7d4cf50f4a720b9d12bf6a3e7557dbd33631
elizabeth189 <elizabethlopez0108@gmail.com>
2023-03-05 22:31:00 -0500

    updated mealroutes

--- controllers/api/mealRoutes.js
+++ controllers/api/mealRoutes.js
@@ -1,40 +1,40 @@
-// Importing 'express' router, Project from models folder, withAuth from utils folder/auth.js file.
+// Importing 'express' router, Meal from models folder, withAuth from utils folder/auth.js file.
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Meal } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // When an http post request is made, this function is called.
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newMeal = await Meal.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
     //(200) success.    //(400) error.
-    res.status(200).json(newProject);
+    res.status(200).json(newMeal);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-// request to delete Project. Protected by Authorization. asyn (request, response)
+// request to delete Meal. Protected by Authorization. asyn (request, response)
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const mealData = await Meal.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    // if projectData not found. Status 404 is sent and running of code ends.
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    // if mealData not found. Status 404 is sent and running of code ends.
+    if (!mealData) {
+      res.status(404).json({ message: 'No meal found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(mealData);
   } catch (err) {
     res.status(500).json(err);
   }
